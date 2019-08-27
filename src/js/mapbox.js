809a1567b9f2504c96946db44354f4fff738e1f3
opeeny godfrey <opeenybusiness@gmail.com>
2019-08-27 10:53:17 +0300

    Refactor index

--- src/js/mapbox.js
+++ src/js/mapbox.js
@@ -1,6 +1,9 @@
 var mapboxgl;
-mapboxgl.accessToken = "pk.eyJ1Ijoib3BlZW55IiwiYSI6ImNqemE5cWw3cDAzdzUzYnFtMW1yNDN0NXkifQ.o90PK6RI-98tnL4hjdhOKA";
-var map = new mapboxgl.Map({
+//var universities = [];//hold universities for filtering
+loadAll();
+function loadAll() {
+  mapboxgl.accessToken = "pk.eyJ1Ijoib3BlZW55IiwiYSI6ImNqemE5cWw3cDAzdzUzYnFtMW1yNDN0NXkifQ.o90PK6RI-98tnL4hjdhOKA";
+  var map = new mapboxgl.Map({
     container: "map",//container id
     style:  "mapbox://styles/mapbox/dark-v10",
     center: [32.590362999999996, 0.31978989999999996],//starting position, Long, Lat
@@ -8,6 +11,24 @@ var map = new mapboxgl.Map({
     maxZoom: 18,
     scrollZoom: false
 });
+map.on('load', function(e) {
+	//alert('what is this');
+	mark();
+});
+/* Layers */
+map.on('load', function(){
+  map.addLayer({
+      id: 'terrain-data',
+      type: 'line',
+      source: {
+          type: 'vector',
+          url: 'mapbox://mapbox.mapbox-terrain-v2'
+      },
+      'source-layer': 'contour'
+  });
+});
+}
+
 function rotate(){
     map.easeTo({bearing: 40, duration: 10000, pitch: 0, zoom: 18});
     setTimeout(function(){
@@ -23,23 +44,8 @@ function rotate(){
 map.on('load', function(){
    // rotate()
 });
-map.on('load', function(){
-   // alert('weee..');
-    map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
-    });
-});
-map.on('load', function(e) {
-	//alert('what is this');
-	mark();
-});
-function mark(){
+
+function mark() {
   var geojson = {
     "type": "FeatureCollection",
     "features": [
@@ -76,7 +82,6 @@ function mark(){
     "coordinates": [32.6149588, 0.3281222]
     }
     },
-    
     {
     "type": "Feature",
     "properties": {
@@ -133,8 +138,10 @@ function toggleSide() {
 function closeSide() {
 	//alert('...');
 	document.getElementById("panel").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  loadAll();
 }
+
 function markerere() {
   var item = document.getElementById('mak');
   item.addEventListener('click', function() {
@@ -155,30 +162,30 @@ function markerere() {
       },
       ]
       };
-      var map = new mapboxgl.Map({
-      container: 'map',
-      style:  "mapbox://styles/mapbox/dark-v10",
-      center: [32.590362999999996, 0.31978989999999996],//starting position, Long, Lat,
-      zoom: 13
-      });
-      
-        var geo = geojson.features.forEach(function(marker) {
-          // create a DOM element for the marker
-          var el = document.createElement('div');
-          el.className = 'mak';
-      
-          // add marker to map
-          new mapboxgl.Marker(el)
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(new mapboxgl.Popup({offset: 25})
-          .setHTML("<p>" + marker.properties.message + "</p>"))
-          .addTo(map);
-          });  
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style:  "mapbox://styles/mapbox/dark-v10",
+  center: [32.590362999999996, 0.31978989999999996],//starting position, Long, Lat,
+  zoom: 13
+  });
+  
+    var geo = geojson.features.forEach(function(marker) {
+      // create a DOM element for the marker
+      var el = document.createElement('div');
+      el.className = 'mak';
+  
+      // add marker to map
+      new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(new mapboxgl.Popup({offset: 25})
+      .setHTML("<p>" + marker.properties.message + "</p>"))
+      .addTo(map);
+      });  
 
   });
 
 }//end mak
-function kyambogo(){
+function kyambogo() {
   var item = document.getElementById('kya');
   item.addEventListener('click', function() {
     var geojson = {
@@ -219,7 +226,7 @@ function kyambogo(){
           });  
   });  
 }
-function ndejje(){
+function ndejje() {
   var item = document.getElementById('ndejje');
   item.addEventListener('click', function() {
     var geojson = {
@@ -345,21 +352,28 @@ function kiu(){
   
 }//end kiu
 
-function filterLocation(){
-  var input, filter, ul, a, i, txtValue;
+function filterLocation() {
+  var input, filter, ul, a, i, txtValue, filtered;
   input = document.getElementById("filter");
-  filter = input.value.toUpperCase();
+  filter = input.value.toUpperCase();//converts to
   ul = document.getElementById('myUL');
   li = ul.getElementsByTagName('li');
-  /* Loop thru. all list items, hide those that dont match*/
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName('a')[0];
-    txtValue = a.textContent || a.innerText;
-    if(txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+  input.addEventListener('keyup', function(e) {
+    //alert('painful struggle');
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName('a')[0];
+      txtValue = a.textContent || a.innerText;
+      if(txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      }
+      else {
+        li[i].style.display = 'none';
+      }
     }
-    else {
-      li[i].style.display = 'none';
-    }
-  }
+  });
+  
+}
+function resetFilter() {
+  document.getElementById('filter').value = '';
+  loadAll();
 }
